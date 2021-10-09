60c27245712b0582081295fc28dfe0103285cec4
pranavrokde27 <91458967+pranavrokde27@users.noreply.github.com>
2021-10-09 16:09:01 +0530

    Add files via upload

---
+++ Assignment_02.java
@@ -0,0 +1,10 @@
+class Assignment_02
+{
+
+public static void main(String [] args)
+{
+	int a=74,b=36,result;
+result=a+b;
+System.out.println("sum of 74 and 36 is = "+result);
+}
+}
---
+++ Assignment_03.java
@@ -0,0 +1,9 @@
+class Assignment_03
+{
+public static void main(String [] args)
+{
+int a=50,b=3,r;
+r=a/b;
+System.out.println("result is = "+r);
+}
+}
